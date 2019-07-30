388eb67184bc4d27ef261d111213d0af22f843c9
Nathan Ribeiro <nathan.f.ribeiro@live.com>
2019-07-29 22:39:12 -0300

    Criação de entidades, upload de arquivos e notificação.

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,60 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
+/**
+ * Non-Authenticated Middlewares
+ */
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+/**
+ * Authentication Middleware
+ */
 routes.use(authMiddleware);
 
+/**
+ * Users
+ */
 routes.put('/users', UserController.update);
+
+/**
+ * Providers
+ */
+routes.get('/providers', ProviderController.index);
+
+/**
+ * Files
+ */
+routes.post('/files', upload.single('file'), FileController.store);
+
+/**
+ * Appointment
+ */
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+/**
+ * Schedule
+ */
+routes.get('/schedule', ScheduleController.index);
+
+/**
+ * Notification
+ */
+routes.get('/notifications', NotificationController.index);
 
 export default routes;
