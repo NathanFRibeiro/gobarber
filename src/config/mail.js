f14baee80a3d93633bb215022129411c8eb0346f
Nathan Ribeiro <nathan.f.ribeiro@live.com>
2019-08-02 22:56:36 -0300

    Envio de e-mail com Nodemailer via Mailtrap pronto

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5be4c4a96405a5',
+    pass: '070f64ce12f273',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
